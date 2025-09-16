48a09a9c84b6cda7503ad7723d5bb40bcf947974
VICTOR <victor.cai@oocl.com>
2025-09-16 11:20:49 +0800

    feat: get /todos with one todo

--- src/main/java/org/example/todo/Todo.java
+++ src/main/java/org/example/todo/Todo.java
@@ -1,4 +1,24 @@
 package org.example.todo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
+    @Id
+    private String id;
+    private String text;
+    private Boolean done;
+
+    @PrePersist
+    public void ensureId() {
+        if (this.id == null) {
+            this.id = java.util.UUID.randomUUID().toString();
+        }
+    }
 }
--- src/test/java/org/example/todo/TodoControllerTests.java
+++ src/test/java/org/example/todo/TodoControllerTests.java
@@ -1,9 +1,11 @@
 package org.example.todo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
@@ -16,15 +18,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TodoControllerTests {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @BeforeEach
+    public void setup() {
+        todoRepository.deleteAll();
+    }
 
     @Test
     void should_response_empty_list_when_index_with_no_any_todo() throws Exception {
         MockHttpServletRequestBuilder request = get("/todos")
-                .contentType("application/json");
+                .contentType(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    @Test
+    void should_response_one_todo_when_index_with_one_todo() throws Exception {
+        Todo todo = new Todo(null, "Buy milk", false);
+        todoRepository.save(todo);
+
+        MockHttpServletRequestBuilder request = get("/todos")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].id").exists())
+                .andExpect(jsonPath("$[0].text").value(todo.getText()))
+                .andExpect(jsonPath("$[0].done").value(todo.getDone()));
     }
 }
