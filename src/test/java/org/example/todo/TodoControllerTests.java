e9a9896cb38328322d1ef65da9666fe0ef79448e
VICTOR <victor.cai@oocl.com>
2025-09-16 10:59:39 +0800

    feat: get /todos with no todo

---
+++ src/test/java/org/example/todo/TodoControllerTests.java
@@ -0,0 +1,30 @@
+package org.example.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_response_empty_list_when_index_with_no_any_todo() throws Exception {
+        MockHttpServletRequestBuilder request = get("/todos")
+                .contentType("application/json");
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+}
